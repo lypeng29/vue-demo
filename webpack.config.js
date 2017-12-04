c82b9fe08957120300ea62218842288cd6ae8505
lypeng <893371810@qq.com>
2017-12-04 17:19:03 +0800

    first

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 module.exports = {
-	entry:'./src/main.js',
-	output:{
-		path:'./dist',
-		filename:'build.js'
-	},
-	module:{
-		loaders:[
-			{
-				test:/\.js$/,
-				loader:'babel',
-				exclude:/node_modules/
-			}
-		]
-	},
-	babel:{
-		"presets":['es2015','stage-0'],
-		"plugins":['transform-runtime']
-	}
-}
+  entry: {
+  	app: './src/index.js',
+  	// print: './src/print.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+  	contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+  	new CleanWebpackPlugin(['dist']),	//传递清空的目录，数组形式
+  	new HtmlWebpackPlugin({
+  		title: 'output management'
+  	}),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  }
+   module: {
+     rules: [
+        {
+          test: /\.css$/,
+          use: [
+          'style-loader',
+          'css-loader'
+          ]
+        },
+        {
+          test: /\.html$/,
+          use: [
+          'html-loader'
+          ]
+        },        
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: [
+          'file-loader'
+          ]
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          use: [
+          'file-loader'
+          ]
+        }              
+     ]
+   }
+};
