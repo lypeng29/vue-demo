7d2f9ed8e514b4ed1226049b03fc4ea6d0c4a757
lypeng <893371810@qq.com>
2017-12-29 16:11:24 +0800

    第一阶段，暂告一段落(v-if bind for click...)

--- README.md
+++ README.md
@@ -1,14 +1,23 @@
 # webpack打包vue
-经历挫折后，总算是打包成功了，可是修改起来还是不熟练，不知道怎么运用到项目中，目前只是打包一个页面~
+经历挫折后，总算是打包成功了，可是修改起来还是不熟练，目前只是打包一个页面，不知道怎么打包多个~学习ing~~
 
-## 运行流程
-1. npm install //安装所需包
-2. npm run build //构建打包bundle.js到dist目录
-3. 浏览器访问根目录index.html文件 //访问效果
+webpack官方文档：https://webpack.js.org/guides/getting-started/
+vue官方文档：https://cn.vuejs.org/v2/guide/
+demo的git地址：https://www.github.com/lypeng29/vue-demo
+
+## 本demo的体验运行流程
+
+*** 注：步骤三，任选一个执行 ***
+
+1. git clone https://www.github.com/lypeng29/vue-demo
+2. npm install //安装所需包
+3. npm run build //构建打包到dist目录，需要自行打开dist/index.html预览
+3. npm run start //构建打包到内存，同时自动打开：http://localhost:5000
+4. 浏览器访问根目录index.html文件 //访问效果
 
 ## 问题汇总
 
-### 安装报错，操作不允许，没有权限
+1. 安装报错，操作不允许，没有权限
 ```bash
 error stack: 'Error: EPERM: operation not permitted, rmdir \'E:\\www\\test\\vue-demo\\node_modules\\fsevents\\node_modules\'',
 error errno: -4048,
@@ -20,9 +29,7 @@ error Please try running this command again as root/Administrator.
 解决：给node_module文件夹，增加一个Everyone，拥有所有权限！如下图：
 
 
-### 安装警告：fsevents跳过了
-
-#### 警告如下：
+2. 安装警告：fsevents跳过了
 
 ```bash
 npm WARN optional SKIPPING OPTIONAL DEPENDENCY: fsevents@^1.0.0 (node_modules\chokidar\node_modules\fsevents):
@@ -31,20 +38,22 @@ npm WARN vue-loader-demo@1.0.0 No description
 npm WARN vue-loader-demo@1.0.0 No repository field.
 ```
 
-#### 解决说明
-fsevent是mac osx系统的，在win或者Linux下使用了 所以会有警告，忽略即可。意思就是你已经安装成功了。
+解决说明：fsevent是mac osx系统的，在win或者Linux下使用了 所以会有警告，忽略即可。意思就是你已经安装成功了。
 
 ### runtime-only build错误
 
-#### 错误如下：
+3. 错误如下：
 ```bash
 [Vue warn]: You are using the runtime-only build of Vue where the template compiler is not available. Either pre-compile the templates into render functions, or use the compiler-included build.
 ```
-#### 解决：
-在webpack.config.js增加以下代码，加载node_module/vue/dist/vue.js文件
+解决：在webpack.config.js增加以下代码，加载node_module/vue/dist/vue.js文件
 resolve: {
     alias: {
         'vue': 'vue/dist/vue.js'
     }
 },
+
+4. 关于Hot Module Replacement
+运行npm run start后，成功打开localhost:5000，但页面是空的，提示：cannot GET /
+解决：在webpack配置中，output路径错了，应该是publicPath: '/'，而不是'./'
 
--- src/app.vue
+++ src/app.vue
@@ -1,35 +1,46 @@
 <template>
-	<div>
-		<pre>
-			* v-text innerText
-			* v-html innerHTML
-			* v-if 移除或者插入
-			* v-show display:none与否
-			* v-model 双向绑定
-			* v-bind 单向，内存JS影响页面
-		</pre>
-		v-text
-		<span v-text="text"></span>
-		<hr/>
-		v-if:
-		<span v-html="html"></span>
-		<hr/>
-		v-if:
-		<div v-if="isShow" style="height:20px;background:#f00;"></div>
-		<hr/>
-		v-model:<br/>
-		<input type="text" name="" v-model="mTest" /><br/>
-		{{mTest}}<br/>
-		<input type="text" name="" v-bind:value="mTest" />
-		<hr/>
-		v-bind:（v-bind:可以简写为:）
-		<div v-bind:class="isRed?'red':'green'">单个class</div>
-		<div :class="{'red':true,'big':true}">多个class</div>
-		根据成绩匹配颜色123<br/>
-		<ul>
-			<li v-for="stu in stus" :class="{'A':'red','B':'green'}[stu.score]">{{stu.name}}</li>
-		</ul>
-	</div>
+<div>
+<pre>
+	* v-text innerText
+	* v-html innerHTML
+	* v-if 移除或者插入
+	* v-show display:none与否
+	* v-model 双向绑定
+	* v-bind 单向，内存JS影响页面，v-bind:class=""可以简写为:class
+	* v-on:click=change()简写：@click=change，没有参数，可以省略括号，但是没有括号，看着不像个函数，我还是保留
+</pre>
+	v-text
+	<span v-text="text"></span>
+	<hr/>
+	v-if:
+	<span v-html="html"></span>
+	<hr/>
+	v-if:
+	<button v-on:click="show_hidden()">显示/影藏</button>
+	<div v-if="isShow" style="height:20px;background:#f00;"></div>
+	<hr/>
+	v-model:<br/>
+	<input type="text" name="" v-model="mTest" /><br/>
+	{{mTest}}<br/>
+	<input type="text" name="" v-bind:value="mTest" />
+	<hr/>
+	v-bind:（v-bind:可以简写为:）
+	<div v-bind:class="isRed?'red':'green'">单个class</div>
+	<div :class="{'red':true,'big':true}">多个class</div>
+	根据成绩匹配颜色123<br/>
+	<ul>
+		<!-- 遍历数组 -->
+		<li v-for="(stu,abc) in stus" :class="{'A':'red','B':'green'}[stu.score]">
+		{{abc}} {{stu.name}}
+		</li>
+		<!-- 遍历对象 -->
+		<li v-for="(value,key,index) in objs">
+		{{index}} {{key}} {{value}}
+		</li>
+	</ul>
+	<!-- <button v-on:click="isRed = !isRed">换色</button>-->
+	<button @click="change()">换色与增加列</button>
+</div>
 </template>
 
 <script>
@@ -46,7 +57,17 @@
 				isShow: true,
 				mTest: '测试',
 				isRed:false,
-				stus:[{'name':'jack','score':'A'},{'name':'rose','score':'B'}]
+				stus:[{'name':'jack','score':'A'},{'name':'rose','score':'B'}],
+				objs:{'name':'mick','age':28,'sex':1,'address':'shannxi'}
+			}
+		},
+		methods:{	
+			change(){
+				this.isRed = !this.isRed;
+				this.stus.push({'name':'lily','score':'A'},{'name':'lucy','score':'B'})
+			},
+			show_hidden(){
+				this.isShow = !this.isShow;
 			}
 		}
 	}
